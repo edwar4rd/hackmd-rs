36cbbae71d3d7e4ead168dd170ae6c31bfd618c8
edwar4rd <edwar4rd@gmail.com>
2024-09-11 02:11:17 +0800

    docs: utilize history endpoint in get-me example

--- examples/get-me.rs
+++ examples/get-me.rs
@@ -1,9 +1,23 @@
-use std::env;
 use hackmd_rs::{context::Context, user::User};
+use std::env;
 
 #[tokio::main]
 async fn main() {
     let context = Context::new(&env::var("HACKMD_TOKEN").expect("HACKMD_TOKEN not found!"));
-    let me = User::me(&context).await.expect("Failed to parse result");
+    let me = User::me(&context).await.expect("Failed getting user info");
     println!("User {}: {:?}", me.name, me);
+
+    let latest = User::get_history(&context)
+        .await
+        .expect("Failed getting user history");
+    if latest.is_empty() {
+        return;
+    }
+
+    println!("");
+
+    println!("Latest read notes:\n");
+    for note in latest {
+        println!("\t{} ({})", note.title, note.publish_link);
+    }
 }
