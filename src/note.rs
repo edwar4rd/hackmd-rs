8e633ad8c7ef1736e59c97a4135040145edfcf34
edwar4rd <edwar4rd@gmail.com>
2024-09-11 01:43:38 +0800

    fea!t: update note related api

--- src/note.rs
+++ src/note.rs
@@ -2,7 +2,8 @@ use serde::{Deserialize, Serialize};
 
 use crate::{
     context::Context,
-    error::{Error, Result},
+    error::Result,
+    permisson::{CommentPermisson, RWPermission},
     user::SimplifiedUser,
 };
 
@@ -15,7 +16,7 @@ pub struct Note {
     pub created_at: usize,
     // TODO: should be an enum, not enough documentation
     pub publish_type: String,
-    pub published_at: Option<String>,
+    pub published_at: Option<usize>,
     pub permalink: Option<String>,
     pub short_id: String,
     pub content: String,
@@ -23,8 +24,9 @@ pub struct Note {
     pub last_changed_user: Option<SimplifiedUser>,
     pub user_path: String,
     pub team_path: Option<String>,
-    pub read_permission: String,
-    pub write_permission: String,
+    pub read_permission: RWPermission,
+    pub write_permission: RWPermission,
+    pub publish_link: String,
 }
 
 impl Note {
@@ -34,41 +36,66 @@ impl Note {
         context.get(&path).await
     }
 
-    pub async fn update(context: &Context, id: &str, update: &NoteUpdate) -> Result<()> {
-        update.patch(context, id).await
+    pub async fn update(&self, context: &Context, update: &NoteUpdate) -> Result<()> {
+        update.apply(context, &self.id).await
     }
-}
 
-#[derive(Debug, Deserialize, PartialEq)]
-pub struct Notes {
-    #[serde(flatten)]
-    notes: Vec<SimplifiedNote>,
-}
-
-impl Notes {
-    pub async fn all(context: &Context) -> Result<Notes> {
-        context.get("notes").await
+    pub async fn delete(context: &Context, id: &str) -> Result<()> {
+        context.delete(&format!("notes/{id}")).await
     }
 }
 
 #[derive(Debug, Deserialize, PartialEq, Serialize)]
 #[serde(rename_all = "camelCase")]
 pub struct SimplifiedNote {
-    id: String,
-    title: String,
-    tags: Vec<String>,
-    created_at: usize,
+    pub id: String,
+    pub title: String,
+    pub tags: Vec<String>,
+    pub created_at: usize,
     // TODO: should be an enum, not enough documentation
-    publish_type: String,
-    published_at: Option<String>,
-    permalink: Option<String>,
-    short_id: String,
-    last_changed_at: usize,
-    last_changed_user: SimplifiedUser,
-    user_path: String,
-    team_path: Option<String>,
-    read_permission: String,
-    write_permission: String,
+    pub publish_type: String,
+    pub published_at: Option<usize>,
+    pub permalink: Option<String>,
+    pub short_id: String,
+    pub last_changed_at: usize,
+    pub last_changed_user: Option<SimplifiedUser>,
+    pub user_path: String,
+    pub team_path: Option<String>,
+    pub read_permission: RWPermission,
+    pub write_permission: RWPermission,
+    pub publish_link: String,
+}
+
+impl SimplifiedNote {
+    pub async fn get_all(context: &Context) -> Result<Vec<SimplifiedNote>> {
+        context.get("notes").await
+    }
+
+    pub async fn get_complete(&self, context: &Context) -> Result<Note> {
+        Note::get(context, &self.id).await
+    }
+}
+
+#[derive(Debug, Serialize)]
+#[serde(rename_all = "camelCase")]
+pub struct NoteCreate {
+    /// seemed to be unused by hackmd api by now
+    pub title: String,
+    pub content: String,
+    pub read_permission: RWPermission,
+    pub write_permission: RWPermission,
+    pub comment_permission: CommentPermisson,
+    pub permalink: String,
+}
+
+impl NoteCreate {
+    pub async fn execute(mut self, context: &Context) -> Result<Note> {
+        if self.read_permission > self.write_permission {
+            self.write_permission = self.read_permission;
+        }
+
+        context.post("notes", &self).await
+    }
 }
 
 #[derive(Debug, Serialize)]
@@ -77,11 +104,15 @@ pub struct NoteUpdate {
     #[serde(skip_serializing_if = "Option::is_none")]
     pub content: Option<String>,
     #[serde(skip_serializing_if = "Option::is_none")]
-    pub read_permission: Option<String>,
+    pub read_permission: Option<RWPermission>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub write_permission: Option<RWPermission>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub permalink: Option<String>,
 }
 
 impl NoteUpdate {
-    pub async fn patch(&self, context: &Context, id: &str) -> Result<()> {
+    pub async fn apply(&self, context: &Context, id: &str) -> Result<()> {
         let path = format!("notes/{id}");
 
         context.patch(&path, self).await
